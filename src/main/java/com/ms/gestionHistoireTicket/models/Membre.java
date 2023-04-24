ae23b76ed4734bcf6224b59a4e87d020e4f58749
nourallah <halloul.nourallah@gmail.com>
2023-04-24 23:23:27 +0100

    modification

--- src/main/java/com/ms/gestionHistoireTicket/models/Membre.java
+++ src/main/java/com/ms/gestionHistoireTicket/models/Membre.java
@@ -4,14 +4,16 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(callSuper=false)
 @ToString
-public class Membre {
+public class Membre extends Personne{
     private Long id ;
 
     @Enumerated(EnumType.STRING)
